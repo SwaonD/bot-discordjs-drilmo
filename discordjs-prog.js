61268f9f3891eb0d4881ea9fbb10da6456f28644
SwaonD <33963606+SwaonD@users.noreply.github.com>
2018-12-10 13:25:09 +0100

    Update discordjs-prog.js

--- discordjs-prog.js
+++ discordjs-prog.js
@@ -46,7 +46,7 @@ client.on('message', async (message) => {
         }
     }
     for (i=0; i<text.commands.createdChannel.length; i++) {
-        if (message.content.startsWith(`${prefix}${text.commands.createdChannel[i]} `)) { // commands
+        if (message.content.startsWith(`${prefix}${text.commands.createdChannel[i]}`)) { // commands
             if (message.channel.id === text.channels.commandChannel.id) {
                 if (message.member.voiceChannel !== undefined) {  
                     if (message.member.voiceChannel.parent !== null) {
